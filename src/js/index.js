7933143d35cb4550ae325128bdf324e649da2693
GeonWooPaeng <gwpaeng@naver.com>
2022-01-12 17:40:48 +0900

    Make part representing lotto number

--- src/js/index.js
+++ src/js/index.js
@@ -21,20 +21,43 @@ $showResultButton.addEventListener("click", onModalShow);
 $modalClose.addEventListener("click", onModalClose);
 
 // ----------------------
+// switch 눌렀을 때 발생
 const $modalNumber = document.querySelector(".modal-number");
+const $modalNumberClose = document.querySelector(".modal-number-close");
 const $switch = document.querySelector(".switch");
 
-export const onModalNumberShow = () => {
-  $modalNumber.classList.add("open");
+const setLottoNumberOnModal = () => {
+  const $lottoNumberTableLastChild = document.querySelector(
+    ".lotto-number-table > tbody"
+  );
+  const $lottoImgs = document.querySelectorAll(".lotto-img");
+
+  $lottoNumberTableLastChild.innerHTML = "";
+  $lottoImgs.forEach(($lottoImg, idx) => {
+    let lottoNums = $lottoImg.dataset.number.split(",").join("-");
+    $lottoNumberTableLastChild.insertAdjacentHTML(
+      "beforeend",
+      `<tr class="text-center>
+        <td class="p-3"></td>
+        <td class="p-3">${idx}</td>
+        <td class="p-3">${lottoNums}</td>
+      </tr>`
+    );
+  });
 };
-export const onModalNumberClose = () => {
+
+const onModalNumberShow = () => {
+  $modalNumber.classList.add("open");
+  setLottoNumberOnModal();
+};
+const onModalNumberClose = () => {
   $modalNumber.classList.remove("open");
 };
 
-$modalClose.addEventListener("click", onModalNumberClose);
+$modalNumberClose.addEventListener("click", onModalNumberClose);
 $switch.addEventListener("click", (event) => {
   if (event.target.checked) {
-    console.log("ddd");
+    onModalNumberShow();
   }
 });
 
@@ -44,19 +67,36 @@ const calCntLottoNums = (money) => {
   return (money - (money % 1000)) / 1000;
 };
 
-const setLottoCnt = (lottoCnt) => {
+const setLottoCntHTML = (lottoCnt) => {
   return `<label class="flex-auto my-0">총 ${lottoCnt}개를 구매하였습니다.</label>`;
 };
 
-const setLottoImg = () => {
-  return `<span class="mx-1 text-4xl">🎟️ </span>`;
+const getRandomNum = () => {
+  let max = 45;
+  let min = 1;
+  return Math.random() * max + min;
+};
+
+const getRandomNumsToString = () => {
+  let num_string = "";
+  for (let i = 0; i < 6; i++) {
+    num_string += Math.floor(getRandomNum());
+    if (i !== 5) num_string += ",";
+  }
+  return num_string;
+};
+
+const setLottoImgHTML = () => {
+  let number = getRandomNumsToString();
+  return `<span class="lotto-img mx-1 text-4xl" data-number="${number}">🎟️ </span>`;
 };
 
 const setPurchasedLottoSection = (lottoCnt) => {
+  //나누기
+  const $purchasedLottoImg = document.querySelector("#purchased-lotto-img");
   const $purchasedLottoSectionDiv = document.querySelector(
     "#purchased-lotto-section > div"
   );
-  const $purchasedLottoImg = document.querySelector("#purchased-lotto-img");
 
   showPurchasedLottoSection();
   $purchasedLottoSectionDiv.removeChild($purchasedLottoSectionDiv.firstChild);
@@ -64,10 +104,10 @@ const setPurchasedLottoSection = (lottoCnt) => {
 
   $purchasedLottoSectionDiv.insertAdjacentHTML(
     "afterbegin",
-    setLottoCnt(lottoCnt)
+    setLottoCntHTML(lottoCnt)
   );
   for (let i = 0; i < lottoCnt; i++) {
-    $purchasedLottoImg.insertAdjacentHTML("beforeend", setLottoImg());
+    $purchasedLottoImg.insertAdjacentHTML("beforeend", setLottoImgHTML());
   }
 };
 
@@ -86,7 +126,7 @@ const init = () => {
   // $lottoNumbersToggleButton.addEventListener("click");
   hidePurchasedLottoSection();
   disabledWinningAndBonusNumber();
-  // disabledResultModalButton();
+  disabledResultModalButton();
 };
 
 export const luckyLotto = () => {
